0c8231fdff2019e6925d7046901e0b77d4021ff2
Nazar <fedunnazar20122001@gmail.com>
2022-09-19 17:30:55 +0300

    create entity data creation

---
+++ ThreadsConsole/DataLibrary/MyDataContext.cs
@@ -0,0 +1,23 @@
+﻿using DataLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    public class MyDataContext : DbContext
+    {
+        public MyDataContext()
+        {
+            this.Database.Migrate(); // щоб автоматично було Update-Database 
+        }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Integrated Security=True;Initial Catalog=UserService"); // строка підключення до БД
+        }
+    }
+}
