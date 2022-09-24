8196b4b11a4fd3c78bde470efef76e5d34d1efdd
Nazar <fedunnazar20122001@gmail.com>
2022-09-24 14:07:59 +0300

    Adding users to DB are done

--- ThreadsConsole/Services/UserService.cs
+++ ThreadsConsole/Services/UserService.cs
@@ -5,33 +5,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bogus;
+using System.Net;
+using DataLibrary.Entities;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace Services
 {
-    public delegate void AddCurrentUserDelegate(int count); //Вказівник на функцію яка приймає ціле число і нічого не повертає
+    public delegate void AddCurrentUserDelegate(int count, ManualResetEvent manualResetEvent, CancellationToken token); //Вказівник на функцію яка приймає ціле число і нічого не повертає
     /// <summary>
     /// Керування користувачами
     /// </summary>
     public class UserService
     {
+        private object thisLock = new object();
         public event AddCurrentUserDelegate AddCurrentUserEvent;  // Подія яка спрацьовує при додавані користувача. На цей івент можна підписатись, і ми будемо перевіряти чи хтось підписався
+        private MyDataContext myDataContext;
         public UserService()
         {
-            MyDataContext myDataContext = new MyDataContext();
+            myDataContext = new MyDataContext();
         }
-        public Task InsertRandomUserAsync(int count)
+
+        public Task InsertRandomUserAsync(int count, ManualResetEvent manualResetEvent, CancellationToken token)
         {
-            return Task.Run(() => InsertRandomUser(count));
+            return Task.Run(() => InsertRandomUser(count, manualResetEvent, token));
         }
 
         //Додавання рандомних даних в БД
-        public void InsertRandomUser(int count)
+        public void InsertRandomUser(int count, ManualResetEvent manualResetEvent, CancellationToken token)
         {
-            for (int i = 0; i < count; i++)
+            //for (int i = 0; i < count; i++)
+            //{
+            //    Thread.Sleep(100);
+            //    if (AddCurrentUserEvent != null) // якщо на цей івент хтось підписався
+            //        AddCurrentUserEvent(i + 1); // то ми в цей івент передаємо кількість елементів - які ми додали 
+            //}
+            lock (thisLock)
             {
-                Thread.Sleep(100);
-                if (AddCurrentUserEvent != null) // якщо на цей івент хтось підписався
-                    AddCurrentUserEvent(i + 1); // то ми в цей івент передаємо кількість елементів - які ми додали 
+                Faker<User> faker = new Faker<User>()
+                .RuleFor(u => u.FirstName, (f, u) => f.Name.FirstName())
+                .RuleFor(u => u.LastName, (f, u) => f.Name.LastName())
+                .RuleFor(u => u.Phone, (f, u) => f.Phone.PhoneNumber())
+                .RuleFor(u => u.Email, (f, u) => f.Internet.Email());
+                for (int i = 0; i < count; i++)
+                {
+                    if (AddCurrentUserEvent != null)
+                    {
+                        manualResetEvent.WaitOne(Timeout.Infinite);
+                        Thread.Sleep(500);
+                        if (token.IsCancellationRequested)
+                        {
+                            myDataContext.SaveChanges();
+                            AddCurrentUserEvent(i, manualResetEvent, token);
+                            return;
+                        }
+                        User user = faker.Generate();
+                        myDataContext.Users.Add(user);
+                        AddCurrentUserEvent(i + 1, manualResetEvent, token);
+                    }
+                }
+                myDataContext.SaveChanges();
             }
         }
     }
--- ThreadsConsole/WPFTaskHW/MainWindow.xaml.cs
+++ ThreadsConsole/WPFTaskHW/MainWindow.xaml.cs
@@ -43,15 +43,24 @@ namespace WPFTaskHW
     public partial class MainWindow : Window
     {
         private object thisLock = new object();
-        private CancellationTokenSource ctSource;
-        private CancellationTokenSource ctSource2; // for label "lbDownload" view
+        private CancellationTokenSource ctSourcePhoto;
+        private CancellationTokenSource ctSourcePhoto2; // for label "lbPhotoDownloading" view
+        private CancellationTokenSource ctSourceUser;
+        private CancellationTokenSource ctSourceUser2; // for label "lbUserDownloading" view
         private CancellationToken token;  // треба для скасування любого потока який захочем
         private CancellationToken token2;
+        private CancellationToken token3;
+        private CancellationToken token4;
         private Task photoDownloadTask;
-        private Task lbPhotoDownloadTask; // for label "lbDownload" view
+        private Task lbPhotoDownloadTask; // for label "lbPhotoDownloading" view
+        private Task userDownloadTask;
+        private Task lbUserDownloadTask; // for label "lbUserDownloading" view
         private static ManualResetEvent photoDownloadMRE = new ManualResetEvent(false); // Вміє лочить потік. Тіпа ставити на паузу
         private static ManualResetEvent lbPhotoDownloadMRE = new ManualResetEvent(false); // Вміє лочить потік. Тіпа ставити на паузу
-        private bool isPaused = false;
+        private static ManualResetEvent userDownloadMRE = new ManualResetEvent(false); // Вміє лочить потік. Тіпа ставити на паузу
+        private static ManualResetEvent lbUserDownloadMRE = new ManualResetEvent(false); // Вміє лочить потік. Тіпа ставити на паузу
+        private bool isPausedPhoto = false;
+        private bool isPausedUser = false;
         private int photoCount = 0;
         private int userCount = 0;
         public MainWindow()
@@ -64,7 +73,7 @@ namespace WPFTaskHW
 
         private void checkImageForlder() // creates image directory if it doesn't exist
         {
-            string path = Directory.GetCurrentDirectory() + "\\Images";
+            string path = Directory.GetCurrentDirectory() + "\\Photo Images";
             if (Directory.Exists(path)) return;
             Directory.CreateDirectory(path);
         }
@@ -170,22 +179,22 @@ namespace WPFTaskHW
 
         private void btnPhotoDownload_Click(object sender, RoutedEventArgs e)
         {
-            lbPhotoDownloading.Visibility = Visibility.Visible;
-            txtPhotoCounter.IsEnabled = false;
-            btnPhotoCancel.IsEnabled = true;
-            btnPhotoPause.IsEnabled = true;
-            btnPhotoDownload.IsEnabled = false;
-            ctSource = new CancellationTokenSource();
-            ctSource2 = new CancellationTokenSource();
-            token = ctSource.Token;
-            token2 = ctSource2.Token;
+            this.Dispatcher.Invoke(() => { lbPhotoDownloading.Visibility = Visibility.Visible; });
+            this.Dispatcher.Invoke(() => { txtPhotoCounter.IsEnabled = false; });
+            this.Dispatcher.Invoke(() => { btnPhotoCancel.IsEnabled = true; });
+            this.Dispatcher.Invoke(() => { btnPhotoPause.IsEnabled = true; });
+            this.Dispatcher.Invoke(() => { btnPhotoDownload.IsEnabled = false; });
+            ctSourcePhoto = new CancellationTokenSource();
+            ctSourcePhoto2 = new CancellationTokenSource();
+            token = ctSourcePhoto.Token;
+            token2 = ctSourcePhoto2.Token;
             photoCount = int.Parse(txtPhotoCounter.Text);
 
             photoDownloadTask = new Task(() => DownloadImages(photoCount), token);
             photoDownloadTask.Start();
             photoDownloadMRE.Set();
 
-            lbPhotoDownloadTask = new Task(() => lbDownloadView(), token2);
+            lbPhotoDownloadTask = new Task(() => lbPhotoDownloadView(), token2);
             lbPhotoDownloadTask.Start();
             lbPhotoDownloadMRE.Set();
             
@@ -193,28 +202,46 @@ namespace WPFTaskHW
 
         private void btnUserDownload_Click(object sender, RoutedEventArgs e)
         {
-            //User_Service.UserService userService = new User_Service.UserService();
-            //userService.AddCurrentUserEvent += UserService_AddCurrentUserEvent; // так ми робим підписку
-            //userService.InsertRandomUserAsync(100); // на ружу повідомляємо скільки користувачів буде заінсерчено
+            this.Dispatcher.Invoke(() => { btnUserDownload.IsEnabled = false; });
+            this.Dispatcher.Invoke(() => { btnUserPause.IsEnabled = true; });
+            this.Dispatcher.Invoke(() => { btnUserCancel.IsEnabled = true; });
+            this.Dispatcher.Invoke(() => { txtUserCounter.IsEnabled = false; });
+            this.Dispatcher.Invoke(() => { lbUserDownloading.Visibility = Visibility.Visible; });
+            ctSourceUser = new CancellationTokenSource();
+            ctSourceUser2 = new CancellationTokenSource();
+            token3 = ctSourceUser.Token;
+            token4 = ctSourceUser2.Token;
             UserService userService = new UserService();
-            userService.AddCurrentUserEvent += UserService_AddCurrentUserEvent;
-            userService.InsertRandomUserAsync(100);
+            userDownloadMRE.Set();
+            userService.AddCurrentUserEvent += UserService_AddCurrentUserEvent; // так ми робим підсику
+            userService.InsertRandomUserAsync(userCount, userDownloadMRE, token3); // на ружу повідомляємо скільки користувачів буде заінсерчено
         }
 
-        private void UserService_AddCurrentUserEvent(int count)
+        private void UserService_AddCurrentUserEvent(int count, ManualResetEvent manualResetEvent, CancellationToken token)
         {
             // і тут ми на UI можем виводити інформацію ту, що робиться в сервісі
+            if (token.IsCancellationRequested)
+            {
+                ResetUserForm(count);
+                return;
+            }
+            else if (count == userCount)
+            {
+                ResetUserForm(count);
+                return;
+            }
+            this.Dispatcher.Invoke(() => { userProgressBar.Value += 1; });
             this.Dispatcher.Invoke(() => { lbUserCounter.Content = $"{count}/{userCount}"; });
         }
 
-        private void lbDownloadView()
+        private void lbPhotoDownloadView()
         {
             while (true)
             {
                 lbPhotoDownloadMRE.WaitOne(Timeout.Infinite);
                 if (token.IsCancellationRequested)
                 {
-                    ctSource2.Cancel();
+                    ctSourcePhoto2.Cancel();
                     return;
                 }
                 Thread.Sleep(500);
@@ -231,7 +258,7 @@ namespace WPFTaskHW
         private void btnPhotoCancel_Click(object sender, RoutedEventArgs e)
         {
             MessageBoxResult res = MessageBoxResult.Yes;
-            if (isPaused)
+            if (isPausedPhoto)
             {
                 this.Dispatcher.Invoke(() => { res = MessageBox.Show("Are you sure?", "Cancel", MessageBoxButton.YesNo, MessageBoxImage.Stop); });
                 if (res == MessageBoxResult.No)
@@ -241,26 +268,52 @@ namespace WPFTaskHW
                 }
                 btnPhotoDownload.IsEnabled = true;
                 photoDownloadMRE.Set();
-                isPaused = !isPaused;
-                ctSource.Cancel();
+                isPausedPhoto = !isPausedPhoto;
+                ctSourcePhoto.Cancel();
             }
             else
             {
                 photoDownloadMRE.Reset();
                 lbPhotoDownloadMRE.Reset();
-                isPaused = !isPaused;
+                isPausedPhoto = !isPausedPhoto;
                 this.Dispatcher.Invoke(() => { res = MessageBox.Show("Are you sure?", "Cancel", MessageBoxButton.YesNo, MessageBoxImage.Stop); });
                 photoDownloadMRE.Set();
                 lbPhotoDownloadMRE.Reset();
-                isPaused = !isPaused;
+                isPausedPhoto = !isPausedPhoto;
                 if (res == MessageBoxResult.No) return;
-                ctSource.Cancel();
+                ctSourcePhoto.Cancel();
             }
+
         }
 
         private void btnUserCancel_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBoxResult res = MessageBoxResult.Yes;
+            if (isPausedUser)
+            {
+                this.Dispatcher.Invoke(() => { res = MessageBox.Show("Are you sure?", "Cancel", MessageBoxButton.YesNo, MessageBoxImage.Stop); });
+                if (res == MessageBoxResult.No)
+                {
+                    lbUserDownloadMRE.Set();
+                    return;
+                }
+                btnUserDownload.IsEnabled = true;
+                userDownloadMRE.Set();
+                isPausedUser = !isPausedUser;
+                ctSourceUser.Cancel();
+            }
+            else
+            {
+                userDownloadMRE.Reset();
+                lbUserDownloadMRE.Reset();
+                isPausedUser = !isPausedUser;
+                this.Dispatcher.Invoke(() => { res = MessageBox.Show("Are you sure?", "Cancel", MessageBoxButton.YesNo, MessageBoxImage.Stop); });
+                userDownloadMRE.Set();
+                lbUserDownloadMRE.Reset();
+                isPausedUser = !isPausedUser;
+                if (res == MessageBoxResult.No) return;
+                ctSourceUser.Cancel();
+            }
         }
 
         private void DownloadImages(int count)
@@ -282,7 +335,7 @@ namespace WPFTaskHW
                     using (WebClient client = new WebClient())
                     {
                         string fileName = Path.GetRandomFileName();
-                        string path = Directory.GetCurrentDirectory() + $"\\Images\\{fileName}.png";
+                        string path = Directory.GetCurrentDirectory() + $"\\Photo Images\\{fileName}.png";
                         client.DownloadFile(new Uri(faker.Image.LoremFlickrUrl()), path);
                     }
                     this.Dispatcher.Invoke(() =>
@@ -312,9 +365,23 @@ namespace WPFTaskHW
             MessageBox.Show($"{count} of {this.photoCount} photos are successfully downloaded!", "SUCCESS DOWNLOAD", MessageBoxButton.OK, MessageBoxImage.Exclamation);
         }
 
+        private void ResetUserForm(int count)
+        {
+            this.Dispatcher.Invoke(() => { lbUserDownloading.Visibility = Visibility.Hidden; });
+            this.Dispatcher.Invoke(() => { txtUserCounter.IsEnabled = true; });
+            this.Dispatcher.Invoke(() => { btnUserCancel.IsEnabled = false; });
+            this.Dispatcher.Invoke(() => { btnUserPause.IsEnabled = false; });
+            this.Dispatcher.Invoke(() => { btnUserPause.Content = "Pause"; });
+            this.Dispatcher.Invoke(() => { btnUserDownload.IsEnabled = true; });
+            this.Dispatcher.Invoke(() => { lbUserCounter.Content = "0/0"; });
+            this.Dispatcher.Invoke(() => { userProgressBar.Value = 0; });
+            this.Dispatcher.Invoke(() => { txtUserCounter.Clear(); });
+            MessageBox.Show($"{count} of {this.userCount} users are successfully added to database!", "SUCCESS DOWNLOAD", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
         private void btnPhotoPause_Click(object sender, RoutedEventArgs e)
         {
-            if (isPaused) // Якщо потік був залочений
+            if (isPausedPhoto) // Якщо потік був залочений
             {
                 photoDownloadMRE.Set(); // Пускаємо потік далі, змінюємо кнопку 
                 this.Dispatcher.Invoke(() => { btnPhotoPause.Content = "Pause"; });
@@ -324,12 +391,22 @@ namespace WPFTaskHW
                 photoDownloadMRE.Reset(); // Залочити потік
                 this.Dispatcher.Invoke(() => { btnPhotoPause.Content = "Continue"; });
             }
-            isPaused = !isPaused;
+            isPausedPhoto = !isPausedPhoto;
         }
 
         private void btnUserPause_Click(object sender, RoutedEventArgs e)
         {
-
+            if (isPausedUser)
+            {
+                userDownloadMRE.Set();
+                this.Dispatcher.Invoke(() => { btnUserPause.Content = "Pause"; });
+            }
+            else
+            {
+                userDownloadMRE.Reset(); // Залочити потік
+                this.Dispatcher.Invoke(() => { btnUserPause.Content = "Continue"; });
+            }
+            isPausedUser = !isPausedUser;
         }
     }
 }
